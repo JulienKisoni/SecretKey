25569e83a7127e7c9a7d2515aa1e40bdec632f43
Julien Kisoni <julienkisonica@gmail.com>
2024-12-03 18:01:24 -0500

    Initial Commit

---
+++ SecretKey/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SecretKey
+//
+//  Created by Julien Kisoni on 2024-12-03.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
