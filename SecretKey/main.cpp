957c7914d7c9f15309ba5d09234ee5e38fe19bbe
Julien Kisoni <julienkisonica@gmail.com>
2024-12-04 18:12:52 -0500

    Done

--- SecretKey/main.cpp
+++ SecretKey/main.cpp
@@ -1,14 +1,37 @@
-//
-//  main.cpp
-//  SecretKey
-//
-//  Created by Julien Kisoni on 2024-12-03.
-//
-
 #include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    string input;
+    cout << "Enter the key" << endl;
+    cin >> input;
+    
+    fstream myFile;
+    
+    string filePath = "secretFile.txt";
+    myFile.open(filePath, ios::in);
+    
+    if (myFile.is_open()) {
+        // Read line by line
+        string line;
+        while (getline(myFile, line)) {
+            cout << "This is the line: " << line << endl;
+            
+            if (input == line) {
+                cout << "OK" << endl;
+            } else {
+                cout << "ERR" << endl;
+            }
+        }
+    } else {
+        cout << "File not opened" << endl;
+    }
+    
+    myFile.close();
+    
     return 0;
 }
