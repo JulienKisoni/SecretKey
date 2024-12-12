f4c8d9413b61469fd263b0cc6982cad80d72bf5a
Julien Kisoni <julienkisonica@gmail.com>
2024-12-12 17:33:00 -0500

    using openssl

--- SecretKey/main.cpp
+++ SecretKey/main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+#include <openssl/evp.h>
+#include <openssl/sha.h>
 
 using namespace std;
+
+string hashPassword(const string& password) {
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, password.c_str(), password.size());
+    SHA256_Final(hash, &sha256);
+
+    // Convert hash to a hex string
+    stringstream ss;
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i) {
+        ss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(hash[i]);
+    }
+    return ss.str();
+}
+
+// Function to compare the hashed password with a plaintext password
+bool comparePassword(const string& hashedPassword, const string& plainPassword) {
+    // Hash the plaintext password
+    string hashedInput = hashPassword(plainPassword);
+
+    // Compare the two hashed strings
+    return hashedPassword == hashedInput;
+}
 
 int main(int argc, const char * argv[]) {
     
@@ -19,9 +46,8 @@ int main(int argc, const char * argv[]) {
         // Read line by line
         string line;
         while (getline(myFile, line)) {
-            cout << "This is the line: " << line << endl;
-            
-            if (input == line) {
+            bool equal = comparePassword(line, input);
+            if (equal) {
                 cout << "OK" << endl;
             } else {
                 cout << "ERR" << endl;
